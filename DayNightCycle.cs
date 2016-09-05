80ee78f1fdd73d825943cf7a8f1218f4997e74f2
Enrico Monese <enricomonese@gmail.com>
2016-09-05 22:26:51 +0200

    Base

---
+++ DayNightCycle.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+//[ExecuteInEditMode]
+public class DayNightCycle : MonoBehaviour {
+
+	public Light sun;
+	public float secondsInFullDay = 120f;
+	public float updateRateInSeconds = 5f;
+	[Range(0,1)]
+	public float currentTimeOfDay = 0.45f;
+	public float timeMultiplier = 1f;
+	[Space(10)]
+	public Gradient nightDayColor;
+	public float maxIntensity = 2f;
+	public float minIntensity = 0f;
+	public float minPoint = -0.2f;
+	public float maxBounceIntensity = 1.0f;
+	public float minBounceIntensity = 0.5f;
+	public float maxAmbient = 1f;
+	public float minAmbient = 0f;
+	public float minAmbientPoint = -0.2f;
+	public Gradient nightDayFogColor;
+	public AnimationCurve fogDensityCurve;
+	public float fogScale = 1f;
+	public float exposureMultiplier = 1f;
+	public float dayAtmosphereThickness = 0.4f;
+	public float nightAtmosphereThickness = 0.87f;
+	Skybox sky;
+	Material skyMat;
+
+	#if UNITY_EDITOR
+	[HideInInspector]
+	public bool showSettings = false;
+
+	public void UpdateEditor () {
+		UpdatePosition ();
+		UpdateFX ();		
+	}
+	public void EditorSetup () {
+		sun = GetComponent<Light>();
+		skyMat = RenderSettings.skybox;
+	}
+	#endif
+	void Start() {
+		sun = GetComponent<Light>();
+		skyMat = RenderSettings.skybox;
+		InvokeRepeating ("UpdateCycle", updateRateInSeconds, updateRateInSeconds);
+	}
+	void UpdateCycle () {
+		UpdatePosition();
+		UpdateFX ();
+
+		currentTimeOfDay += ((Time.deltaTime + updateRateInSeconds) / secondsInFullDay) * timeMultiplier;
+
+		if (Input.GetKeyDown (KeyCode.O)) timeMultiplier *= 0.5f;
+		if (Input.GetKeyDown (KeyCode.P)) timeMultiplier *= 2f;
+
+		if (currentTimeOfDay >= 1) {
+			currentTimeOfDay = 0;
+		}
+	}
+
+	void UpdatePosition() {
+		sun.transform.localRotation = Quaternion.Euler((currentTimeOfDay * 360f) - 90, 170, 0);
+	}
+
+	void UpdateFX () {
+		float tRange = 1 - minPoint;
+		float dot = Mathf.Clamp01 ((Vector3.Dot (sun.transform.forward, Vector3.down) - minPoint) / tRange);
+		float i = ((maxIntensity - minIntensity) * dot) + minIntensity;
+		sun.intensity = i;
+
+		i = ((maxBounceIntensity - minBounceIntensity) * dot) + minBounceIntensity;
+		sun.bounceIntensity = i;
+
+		tRange = 1 - minAmbientPoint;
+		dot = Mathf.Clamp01 ((Vector3.Dot (sun.transform.forward, Vector3.down) - minAmbientPoint) / tRange);
+		i = ((maxAmbient - minAmbient) * dot) + minAmbient;
+		RenderSettings.ambientIntensity = i;
+
+		sun.color = nightDayColor.Evaluate(dot);
+		RenderSettings.ambientLight = sun.color;
+
+		RenderSettings.fogColor = nightDayFogColor.Evaluate(dot);
+		RenderSettings.fogDensity = fogDensityCurve.Evaluate(dot) * fogScale;
+
+		i = ((dayAtmosphereThickness - nightAtmosphereThickness) * dot) + nightAtmosphereThickness;
+		skyMat.SetFloat ("_AtmosphereThickness", i);
+		skyMat.SetFloat ("_Exposure", i * exposureMultiplier);
+	}
+}
---
+++ Editor/DayNightCycleEditor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+[CustomEditor (typeof(DayNightCycle))]
+public class DayNightCycleEditor : Editor {
+
+	DayNightCycle script;
+
+	void OnEnable () {
+		script = target as DayNightCycle;
+		script.EditorSetup ();
+	}
+
+
+	public override void OnInspectorGUI () {
+		base.OnInspectorGUI ();
+		SecondsDurationGUI ("Complete day duration: ", script.secondsInFullDay);
+		SecondsDurationGUI ("Enviroment updates every: ", script.updateRateInSeconds);
+		float currentSeconds = script.secondsInFullDay * script.currentTimeOfDay;
+		SecondsDurationGUI ("Elapsed time from : ", currentSeconds);
+		float hour = 86400f * script.currentTimeOfDay;
+		SecondsDurationGUI ("Current in game time: ", hour);
+
+		if (GUI.changed) {
+			EditorUtility.SetDirty (script);
+			script.UpdateEditor ();
+		}
+	}
+
+	void SecondsDurationGUI (string message, float seconds) {
+		System.TimeSpan time = System.TimeSpan.FromSeconds(seconds);
+		string str = string.Format("{0:D2}h:{1:D2}m:{2:D2}s:{3:D2}ms", time.Hours, time.Minutes, time.Seconds, time.Milliseconds);
+		EditorGUILayout.HelpBox (message + str, MessageType.None, true);
+	}
+}
